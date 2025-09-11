d431895d92dbe801a9aef23050b4316a336446cf
jungmin001 <jungmin4556@naver.com>
2025-09-11 20:18:37 +0900

    과제 5번

---
+++ Source/a93/MyActor.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyActor.generated.h"
+
+UCLASS()
+class A93_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AMyActor();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	FVector2D CurrentLocation;
+
+	int32 MoveCount;
+
+	int32 Step();
+
+	void Move();
+};
